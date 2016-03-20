e1e23fa4c6769cb07695a123232e6e4688671d82
刘鑫伟 <liuxinwei1995@gmail.com>
2016-03-20 18:06:32 +0800

    创建项目。

---
+++ DataStructure/src/MergeSort.java
@@ -0,0 +1,14 @@
+/**
+ * 归并排序。
+ * @author 刘鑫伟
+ *
+ */
+public class MergeSort {
+	public static void main(String[] args) {
+		int[] a = {5,3,8,6,1,9,2,7,4};
+		
+		for (int i=0; i<a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+	}
+}
